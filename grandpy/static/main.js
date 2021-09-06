c6bfac99f9f8068718667b326e1107a07f94df7d
le gulluche <plegulluche@gmail.com>
2021-09-06 03:21:13 +0200

    refactor main.js

--- grandpy/static/main.js
+++ grandpy/static/main.js
@@ -1,44 +1,57 @@
-document.querySelector('#submit-button').addEventListener('click', getInput);
+//SELECTORS
+
+const submitButton = document.querySelector('#submit-button');
+const inputField = document.querySelector('#input-text');
+const ulContainer = document.querySelector('#items');
+
+
+//LISTENERS
+submitButton.addEventListener('click', getInput);
+
+
+//FUNCTIONS
 // phrase test : Comment s'est passé ta soirée avec Grandma hier soir? Au fait, pendant que j'y pense, pourrais-tu m'indiquer où se trouve le musée d'art et d'histoire de Fribourg, s'il te plaît?
 function getInput(event) {
     event.preventDefault();
-    console.log('insidegetInput')
-
-    let input = document.querySelector('#input-text').value;
-    let ul = document.querySelector('#items');
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode(input));
-    ul.appendChild(li);
+    //Create li
+    function addLiToUl(value) {
+        let li = document.createElement('li');
+        li.setAttribute('class', 'text-message')
+        li.appendChild(document.createTextNode(value));
+        ulContainer.appendChild(li);
+    }
+    //Select value of input field
+    let inputValue = inputField.value;
+    addLiToUl(inputValue);
+    //Reseting the input field 
     document.querySelector('#my-form').reset();
-    console.log('ending getinput')
+    
 
-    const sentence = document.querySelector('li').textContent;
-
-    if (sentence !== '' && sentence !== undefined) {
+    //Fetch to backend.
+    if (inputValue !== '' && inputValue !== undefined) {
         fetch(`${window.origin}/process`, {
             method: 'POST',
             credentials: 'include',
-            body: JSON.stringify(sentence),
+            body: JSON.stringify(inputValue),
             cache: 'no-cache',
             headers: new Headers({ 'Content-Type': 'application/json'})
         })
         .then((response) => {
             if (response.status !== 200) {
-                let li2 = document.createElement('li');
-                li2.appendChild(document.createTextNode('Désolé mais je n\'ai pas compris ta requète'));
-                ul.appendChild(li2);
-                console.log(`response status Not 200: ${response.status}`);
+                let rep = 'désolé je n\'ai pas compris'
+                addLiToUl(rep)
                 return ;
             }
+
         response.json().then((data) => {
-            const backData = JSON.stringify(data)
-            console.log(backData);
-            let li3 = document.createElement('li');
-            li3.appendChild(document.createTextNode(backData));
-            ul.appendChild(li3);
-        })
+                    backEndData = data   
+                    console.log(data.google_response.lat)
+                    
+            })
+        
         })
     }
-       
+    
+    
 }
 
